e4e90157458530c1a8bbcc560ac2dbee2343879d
xiaowang <996108220@qq.com>
2018-09-18 16:02:15 +0800

    fix-bind-eph

--- src/bind_ephemeral_ip.sh
+++ src/bind_ephemeral_ip.sh
@@ -1,4 +1,3 @@
-local id=$1
-port=`nova interface-list $id|awk 'NR==4{print $4}'`
+port=`nova interface-list $1|awk 'NR==4{print $4}'`
 fixip=`proton port-show $port| grep fixed_ips | awk -F '{"id":' '{print $2}' | awk -F '"' '{print $2}'`
 proton ephemeral-ip-create internet $fixip 1000 1000
